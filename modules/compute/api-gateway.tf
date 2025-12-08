8262d63796f4ec870621678cc3c5701091825f20
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-09 05:15:00 +0800

    api gateway users resource and methods

--- modules/compute/api-gateway.tf
+++ modules/compute/api-gateway.tf
@@ -1,39 +1,44 @@
-resource "aws_api_gateway_rest_api" "rest_api" {
-  name        = "rest-api-gateway"
+resource "aws_api_gateway_rest_api" "main" {
+  name        = "backend-api-${var.environment}"
   description = "REST API Gateway for application"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "version" {
+  for_each = toset(var.api_versions)
+
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = each.value
 }
 
-resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
+resource "aws_api_gateway_resource" "users_versioned" {
+  for_each = toset(var.api_versions)
+
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.version[each.value].id
+  path_part   = "users"
+}
+
+
+# GET /v1/users, GET /v2/users
+resource "aws_api_gateway_method" "get_users" {
+  for_each = toset(var.api_versions)
+
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.users_versioned[each.value].id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "proxy_integration" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  type        = "MOCK"
-  integration_http_method = "ANY"
+resource "aws_api_gateway_integration" "get_users" {
+  for_each = toset(var.api_versions)
+
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.users_versioned[each.value].id
+  http_method = aws_api_gateway_method.get_users[each.value].http_method
+
+  type                    = "HTTP_PROXY"
+  integration_http_method = "GET"
+  uri                     = "${var.backend_endpoint}/${each.value}/users"
+
 }
-
-resource "aws_api_gateway_deployment" "rest_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-
-  depends_on = [
-    aws_api_gateway_integration.proxy_integration
-  ]
-}
-
-output "rest_api_invoke_url" {
-  value       = "https://${aws_api_gateway_rest_api.rest_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.rest_deployment.stage_name}/"
-  description = "Invoke URL of the REST API Gateway endpoint"
-}
-
