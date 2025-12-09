9be1a059f451021dd07877add3d6da7fb1756171
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-12-09 08:05:22 +0800

    module parameters

--- main.tf
+++ main.tf
@@ -11,9 +11,12 @@ provider "aws" {
   region = var.region
 }
 
-
 module "network" {
   source = "./modules/network"
+  vpc_cidr_block = "10.0.0.0/16"
+  public_subnet_cidr_block = "10.0.1.0/24"
+  private_subnet_cidr_block = "10.0.11.0/24"
+  availability_zone = "ap-southeast-1a"
 }
 
 module "compute" {
@@ -21,4 +24,8 @@ module "compute" {
   environment      = "production"
   api_versions     = ["/api/v1", "/api/v2"]
   backend_endpoint = "http://example-backend:8080"
+  instance_type = "t3-micro"
+  
+  ec2_security_group_id = module.network.security_group_id
+  private_subnet_id = module.network.private_subnet_id
 }
