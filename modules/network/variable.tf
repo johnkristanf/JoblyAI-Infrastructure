8e27e88ad60014edcd8ee74849a44d8072d7a5ce
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-12-09 08:06:24 +0800

    remove defaults

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -7,18 +7,15 @@ variable "vpc_cidr_block" {
 variable "public_subnet_cidr_block" {
   description = "CIDR block for the public subnet"
   type        = string
-  default     = "10.0.1.0/24"
 }
 
 
 variable "private_subnet_cidr_block" {
   description = "CIDR block for the public subnet"
   type        = string
-  default     = "10.0.11.0/24"
 }
 
 variable "availability_zone" {
   description = "Availability zone for the public subnet"
   type        = string
-  default     = "ap-southeast-1a"
 }
