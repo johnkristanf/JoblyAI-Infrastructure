4d608ee8a6abb16a5d8028d3f9f26d51cea44123
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-10 05:11:24 +0800

    add key pair and metadata

--- modules/compute/ec2.tf
+++ modules/compute/ec2.tf
@@ -14,36 +14,39 @@ data "aws_ami" "main" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "main" {
+  key_name = "joblyai-key-pair"
+  public_key = file("~/.ssh/joblyai-key-pair.pub")
+}
+
+# If availability zone of this instance is not specified, by default, it 
+# uses the subnet's az
 resource "aws_instance" "main" {
   ami                         = data.aws_ami.main.id
   instance_type               = var.instance_type
   subnet_id                   = var.private_subnet_id
   vpc_security_group_ids      = [var.ec2_security_group_id]
+
   associate_public_ip_address = false
 
-  # Recommended: specify a key name for SSH access
-  key_name = var.key_name
-
-  # Optional: enable root block device configuration
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 16
-    encrypted   = true
-  }
-
-  # Optional: add user_data for bootstrapping (cloud-init, etc.)
-  user_data = file("${path.module}/user_data.sh")
-
-  # Optional: specify EBS optimization
-  ebs_optimized = true
-
-  # Optional: enable/attach instance metadata options for security best practices
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
   }
 
+  # Recommended: specify a key name for SSH access
+  key_name = aws_key_pair.main.key_name
+
+  # Optional: enable root block device configuration
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+    encrypted   = true
+  }
+
+  # Optional: add user_data for bootstrapping (cloud-init, etc.)
+  user_data = file("${path.module}/user_data.sh")
   tags = {
     Name = "main-ec2"
     Environment = var.environment
