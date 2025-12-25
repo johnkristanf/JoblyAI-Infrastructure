78a351cd86466fe7f89bc3e74b23ce89d17e6e53
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-25 15:37:33 +0800

    terraform secrets

--- variable.tf
+++ variable.tf
@@ -1,5 +1,11 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
+  sensitive = true
 }
 
+variable "profile" {
+  description = "The AWS profile"
+  type        = string
+  sensitive = true
+}
